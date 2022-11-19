c081c735b24e74583685a1e544951129d06af4d9
chinch42 <vanessakolund@gmail.com>
2022-11-19 21:11:19 +0100

    update loan range

--- src/Calculate.java
+++ src/Calculate.java
@@ -7,7 +7,7 @@ import java.text.DecimalFormat;
 
 public class Calculate {
     DecimalFormat formatter = new DecimalFormat("#.##");
-    // instance variables
+    // constants
     final double perMonth = 12F * 100F;
     final float classTreshold = 100_000F;
     final float minLoan = 1000;
@@ -21,43 +21,32 @@ public class Calculate {
     double PMT = 0F;// the total monthly mortgage payment
     double r = 0F;// fixed yearly interest rate
     double totalLoanAmount = 0F; // total loan amount
-    boolean elligbile;
-
+    boolean notEliglbe = (this.totalLoanAmount <minLoan || this.totalLoanAmount>maxLoan)?true : false;
     // constructor
-
-    public Calculate(int termIn, double interestRateIN, double totalLoanAmountIn) {
-        this.n = termIn * 12; // Multiply the number of years in your loan term by 12
-        this.PMT = 0.0D;
-        this.r = interestRateIN / perMonth;// r[%] -> absolute monthly rate r/(100*12)
-        this.totalLoanAmount = totalLoanAmountIn;
-    }
-
-    // 2. constructor
     public Calculate(Scanner scan) {
         int i = 0;
+       
         for (String outString : variableString) {
-            keyVal[i] = this.testInputstring(outString, scan);
+            keyVal[i] = this.testInputstring(outString, scan,i);
             i++;
         }
         this.totalLoanAmount = keyVal[0];
         this.r = keyVal[1] / perMonth;
         this.n = (int) keyVal[2] * 12;
-        this.elligbile= this.totalLoanAmount > minLoan ? true:false;
     }
-
     // which category: first or economy?
     public String getClassStatus() {
         this.classStatus = totalLoanAmount > this.classTreshold ? "First" : "Economy";
         return this.classStatus;
     }
-
     // check user input type
-    public float testInputstring(String field, Scanner scan) {
+    public float testInputstring(String field, Scanner scan, int i) {
 
         System.out.print(field + ":");
         String input;
         boolean check = true;
         Float inputFl = 0F;
+
         while (check) {
             input = scan.nextLine();
             if (input.matches("((-|\\+)?[0-9]+(\\.[0-9]+)?)+")) {
@@ -65,7 +54,12 @@ public class Calculate {
                 if (inputFl <= 0.0) {
                     System.out.println(field + " cannot be negative or zero");
                     System.out.print(field + ":");
-                } else {
+                }
+                else if(i==0 && (inputFl < minLoan) || (inputFl > maxLoan)){
+                    System.out.println("Total Amount has to be in range of 1000-1 Mio €.");
+                    System.out.print(field + ":");
+                } 
+                else {
                     check = false;
                 }
             } else if (input.isEmpty()) {
@@ -79,16 +73,14 @@ public class Calculate {
         }
         return inputFl;
     }
-
     // calculate mortgage
     public double fixedPMT() {
-       
+
         // M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1].
         double power = Math.pow((1 + this.r), this.n);
         double res = (this.totalLoanAmount * this.r * power) / (power - 1D);
         this.PMT = Double.parseDouble(formatter.format(res));
         return this.PMT;
-       
     }
 
 };
--- src/Main.java
+++ src/Main.java
@@ -2,47 +2,13 @@ import java.util.Scanner;
 import java.io.InputStreamReader;
 
 public class Main {
-    // function testInputstring
-    public static float testInputstring(String field, Scanner scan) {
-
-        System.out.print(field + ":");
-        String input;
-        boolean check = true;
-        Float inputFl = 0F;
-        while (check) {
-            input = scan.nextLine();
-            if (input.isEmpty()) {
-                System.out.println(field + " cannot be empty:");
-                System.out.print(field + ":");
-                check = true;
-            }
-            if (input.matches("((-|\\+)?[0-9]+(\\.[0-9]+)?)+")) {
-                inputFl = Float.parseFloat(input);
-                if (inputFl <= 0.0) {
-                    System.out.println(field + " cannot be negative or zero ");
-                    System.out.print(field + ":");
-                } else {
-                    check = false;
-                }
-            } else {
-                System.out.println(field + " must be a number ");
-                System.out.print(field + ":");
-            }
-        }
-        return inputFl;
-    }
-
     // Main method
     public static void main(String[] args) {
 
         InputStreamReader rdr = new InputStreamReader(System.in);
         Scanner scan = new Scanner(rdr);
         Calculate obj = new Calculate(scan);
-        if (obj.elligbile) {
-            System.out.println("Your monthly mortgage payment is : " + obj.fixedPMT() + " €.");
-        } else {
-            System.out.println("You are not elligbile. Minimum loan amount has to be > "+(int)obj.minLoan+" €." );
-        }
+        System.out.println("Your monthly mortgage payment is:"+obj.fixedPMT()+"€.");
         scan.close();
     }
 }
